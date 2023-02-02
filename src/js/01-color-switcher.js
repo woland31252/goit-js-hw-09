cfc9090f131c38af6c36793dc91218c21b5b877e
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-02-02 22:47:06 +0200

    update task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timer = null;
 
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+  timer = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function onStop() {
+  clearInterval(timer);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
