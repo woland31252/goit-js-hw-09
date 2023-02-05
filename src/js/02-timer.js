3e6eca133712a9ab97b926abac7b5f3daeb010e9
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-02-05 19:18:36 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,12 +1,16 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-
 const input = document.getElementById("datetime-picker");
-const button = document.querySelector("[data-start]");
-const timer = document.querySelector("timer");
+// const refs = {
+//   button: document.querySelector("[data-start]"),
+//   day: document.querySelector("[data-days]"),
+//   hour: document.querySelector("[data-hours]"),
+//   minute: document.querySelector("[data-minutes]"),
+//   second: document.querySelector("[data-seconds]"),
+// };
 
 // input.addEventListener("input", onNumberData);
-// button.addEventListener("click", onTimerDate);
+button.addEventListener("click", onStart);
 
 const options = {
   enableTime: true,
@@ -15,11 +19,16 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    console.log(options.defaultDate);
+    if (selectedDates[0] < options.defaultDate) {
+      alert('Please choose a date in the future');
+      return;
+    }
   },
 };
 
 const fp = flatpickr(input, options);
-const timerID = setInterval(convertMs, 1000)
+// const timerID = setInterval(convertMs, 1000)
 // function onTimerDate() {
 //   timer = setInterval(() => {
 //     document.body.style.backgroundColor = convertMs;
@@ -27,6 +36,10 @@ const timerID = setInterval(convertMs, 1000)
 //   startBtn.disabled = true;
 //   stopBtn.disabled = false;
 // }
+
+function onStart() {
+  options.defaultDate = setInterval(convertMs, 1000)
+}
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -42,10 +55,16 @@ function convertMs(ms) {
   const minutes = Math.floor(((ms % day) % hour) / minute);
   // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-// timer.textContent = `${days} ${hours} ${minutes} ${seconds}`
-  return { days, hours, minutes, seconds };
+  
+  // return timer .field >'[data-days]'.textContent(`${days}`)
+  return { days, hours, minutes, seconds};
 }
+
 
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+const a = 3;
+const b = 4;
+console.log(a + b);
