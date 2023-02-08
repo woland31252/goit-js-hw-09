992bf7b42945a12ec792b1772a7062b87f1fde65
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-02-08 21:31:26 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,9 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", onPromise);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
